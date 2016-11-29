b5a50c2968cb4986dd319b39c615fc06e943f986
resendefreitas <guilherme.freitas@vtex.com.br>
2016-11-29 15:55:14 -0200

    Cria lista

--- Model.java
+++ Model.java
@@ -1,28 +1,34 @@
 import java.util.GregorianCalendar;
 import javax.swing.*;
+import java.util.LinkedHashMap;
 
 public class Model {
-  private String nome;
-  private int idade;
-  private GregorianCalendar niver;
-  private double temperaturaMedia;
+  private LinkedHashMap Lista;
 
-  public void setNiver(int year, int month, int dayOfMonth){
-    GregorianCalendar niver = new GregorianCalendar(year, month, dayOfMonth);
-    this.niver = niver;
+  public class Cidade {
+    private String nome;
+    private int idade;
+    private GregorianCalendar niver;
+    private double temperaturaMedia;
+
+    public void setNiver(int year, int month, int dayOfMonth){
+      GregorianCalendar niver = new GregorianCalendar(year, month, dayOfMonth);
+      this.niver = niver;
+    }
+
+    public void setIdade(int idade){
+      this.idade = idade;
+    }
+
+    public void setNome(String nome){
+      this.nome = nome;
+    }
+
+    public void setTemperaturaMedia(double temp){
+      this.temperaturaMedia = temp;
+    }
   }
 
-  public void setIdade(int idade){
-    this.idade = idade;
-  }
-
-  public void setNome(String nome){
-    this.nome = nome;
-  }
-
-  public void setTemperaturaMedia(double temp){
-    this.temperaturaMedia = temp;
-  }
 
   public void incluiObjeto(){
     JOptionPane.showMessageDialog(null, "clicou no incluir");
