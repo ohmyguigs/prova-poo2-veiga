2a08b359607eb4269572443d0a5db8757d47dfdd
resendefreitas <guilherme.freitas@vtex.com.br>
2016-11-29 15:21:34 -0200

    Bota uns set

--- Model.java
+++ Model.java
@@ -5,6 +5,24 @@ public class Model {
   private String nome;
   private int idade;
   private GregorianCalendar niver;
+  private double temperaturaMedia;
+
+  public void setNiver(int year, int month, int dayOfMonth){
+    GregorianCalendar niver = new GregorianCalendar(year, month, dayOfMonth);
+    this.niver = niver;
+  }
+
+  public void setIdade(int idade){
+    this.idade = idade;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public void setTemperaturaMedia(double temp){
+    this.temperaturaMedia = temp;
+  }
 
   public void incluiObjeto(){
     JOptionPane.showMessageDialog(null, "clicou no incluir");
